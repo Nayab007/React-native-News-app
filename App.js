5dae0039f43d4fd37ecaf0724d28baa4c4be61bc
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-08-23 20:20:40 +0200

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello Nayab</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#B1D7B4',
     alignItems: 'center',
     justifyContent: 'center',
   },
