60b1162630045296c6bab1a467278a94d8ef96a8
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-08-23 20:23:22 +0200

    add

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#B1D7B2',
+    backgroundColor: '#B1D7B3',
     alignItems: 'center',
     justifyContent: 'center',
   },
