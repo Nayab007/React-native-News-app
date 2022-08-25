e00a8589eebd461561f8462ff4bc95dad90d8d70
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-08-25 09:56:31 +0200

    Update App.js
    update app.js

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from "./screens/HomeScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SearchScreen from "./screens/SearchScreen";
+import { Entypo,FontAwesome } from '@expo/vector-icons';
 
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello Nayab</Text>
-      <StatusBar style="auto" />
-    </View>
+
+        <NavigationContainer>
+            <Tab.Navigator screenOptions={{
+                tabBarActiveTintColor: "red"
+            }}>
+                <Tab.Screen name="Home" component={HomeScreen} options={{
+                    tabBarIcon: () =><Entypo name="home" size={24} color="black" />
+                }}/>
+                <Tab.Screen name="Search" component={SearchScreen} options={{
+                    tabBarIcon: () =><FontAwesome name="search" size={24} color="black" />
+                }}/>
+            </Tab.Navigator>
+        </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#B1D7B4',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
 });
